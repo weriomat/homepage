12ab0b9b4781968044804a25c87738a3f013ac38
weriomat <engel@weriomat.com>
2024-09-25 20:44:40 +0200

    chore: update

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }: (flake-utils.lib.eachDefaultSystem (
+    utils,
+  }: (utils.lib.eachDefaultSystem (
     system: let
       pkgs = import nixpkgs {
         inherit system;
